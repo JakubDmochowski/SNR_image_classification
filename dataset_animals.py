3ff0a94e2e4e30ff298ccc49584475eb74260b6f
kkobylin <k.kobylinski@stud.elka.pw.edu.pl>
2021-12-27 20:53:56 +0100

    Add train function with errors

--- dataset_animals.py
+++ dataset_animals.py
@@ -2,17 +2,14 @@ import os
 import pandas as pd
 from torch.utils.data import Dataset
 from torchvision.io import read_image
+import torch
+
 
 class AnimalsDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, split="70/15/15", transform=None, target_transform=None):
-        s = split.split('/')
-        self.train_p = s[0]
-        self.test_p = s[1]
-        self.validation_p = s[2]
+    def __init__(self, annotations_file, img_dir, transform=None):
         self.img_labels = pd.read_csv(annotations_file)
         self.img_dir = img_dir
         self.transform = transform
-        self.target_transform = target_transform
 
     def __len__(self):
         return len(self.img_labels)
@@ -23,6 +20,8 @@ class AnimalsDataset(Dataset):
         label = self.img_labels.iloc[idx, 1]
         if self.transform:
             image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
+        if image.size()[0] == 4:
+            image = image[0:3, :, :]
+        # if self.target_transform:
+        #     label = self.target_transform(label)
         return image, label
--- main.py
+++ main.py
@@ -1,37 +1,51 @@
 import os
-import torch
-from torchvision import datasets
-from dataset_animals import AnimalsDataset
-import matplotlib.pyplot as plt
-from torch.utils.data import DataLoader
-import torchvision.transforms as T
-from PIL import Image
 
-image_size = (224,224)
-transform = T.Compose([T.ToPILImage(), T.Resize(image_size), T.PILToTensor()])
+
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+from dataset_animals import AnimalsDataset
+import torchvision.models as models
+from pretrainedAlex import train_alex
+import torch
+
+# image_size = (224, 224)
+# transform = transforms.Compose([transforms.ToPILImage(), transforms.Resize(image_size), transforms.PILToTensor()])
+transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize(244),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    # transforms.Normalize(
+    #     mean=[0.485, 0.456, 0.406],
+    #     std=[0.229, 0.224, 0.225]
+    # )
+    ])
 
 training_data = AnimalsDataset(
     annotations_file=f"{os.getcwd()}/datasets/animals10_train",
     img_dir=f"{os.getcwd()}/datasets/Animals-10",
-    split="70/15/15",
     transform=transform
 )
 test_data = AnimalsDataset(
     annotations_file=f"{os.getcwd()}/datasets/animals10_test",
     img_dir=f"{os.getcwd()}/datasets/Animals-10",
-    split="70/15/15",
     transform=transform
 )
 validation_data = AnimalsDataset(
     annotations_file=f"{os.getcwd()}/datasets/animals10_validation",
     img_dir=f"{os.getcwd()}/datasets/Animals-10",
-    split="70/15/15",
     transform=transform
 )
 
-train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
-test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+train_data_loader = DataLoader(training_data, batch_size=64, shuffle=True)
+test_data_loader = DataLoader(test_data, batch_size=64, shuffle=True)
+validation_data_loader = DataLoader(validation_data, batch_size=64, shuffle=True)
 
-train_features, train_labels = next(iter(train_dataloader))
-print(f"Feature batch shape: {train_features.size()}")
-print(f"Labels batch shape: {train_labels.size()}")
+model = models.resnet18(pretrained=True, progress=True)
+print(model)
+# train_alex(train_data_loader, test_data_loader)
+
+# train_features, train_labels = next(iter(train_dataloader))
+# print(f"Feature batch shape: {train_features.size()}")
+# print(f"Labels batch shape: {train_labels.size()}")
